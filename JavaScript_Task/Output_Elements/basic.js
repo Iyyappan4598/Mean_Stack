330a6731fc50d8c3a314c0f2ab0704b3d3396a41
Iyyappan4598 <iyyappan41998@gmail.com>
2023-07-07 11:54:14 +0530

    JavaScript output element Task
    Practice

---
+++ JavaScript_Task/Output_Elements/basic.js
@@ -0,0 +1,8 @@
+// document.getElementById('para').innerHTML = (5 + 10);
+// document.write(5 + 6);
+// console.log("console output element");
+// console.log(20 + 20);
+// window.alert("Warning");
+// window.prompt("Security Warning");
+
+document.getElementById('para').innerHTML = 2 + 4;
